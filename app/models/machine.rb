f40246e8fb2aa111cba456f5e2983be9c3dc0641
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-06 10:06:28 -0600

    Create models for snack and machinesnack and add instance methods to machine model

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,14 @@
 class Machine < ApplicationRecord
   validates_presence_of :location
-  
+  has_many :machine_snacks
+  has_many :snacks, through: :machine_snacks
   belongs_to :owner
+
+  def average_snack_price
+    self.snacks.average(:price)
+  end
+
+  def total_snack_count
+    self.snacks.count
+  end
 end
