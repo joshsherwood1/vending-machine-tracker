6ed134cbd722a98866a573db836ba14b64d116aa
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-06 10:06:28 -0600

    Update seeds file with snacks, machines, and owner objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Snack.destroy_all
+Machine.destroy_all
+Owner.destroy_all
+
+sam = Owner.create(name: "Sam's Snacks")
+mixed_drinks = sam.machines.create(location: "Don's Mixed Drinks")
+basement = sam.machines.create(location: "Turing Basement")
+snack_1 = basement.snacks.create(name: "Cheetos", price: 1.50)
+snack_2 = basement.snacks.create(name: "Chocolate", price: 2.50)
+snack_3 = mixed_drinks.snacks.create(name: "Gatorade", price: 3.50)
+mixed_drinks.snacks << snack_1
