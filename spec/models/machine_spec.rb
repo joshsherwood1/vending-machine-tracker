2f992ea4d74e9af34cd1982b2098cedd908c5965
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-06 10:06:28 -0600

    Add model spec tests for machines, snacks, machine snacks, and tests for machine instance methods
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,5 +4,27 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it {should have_many :machine_snacks}
+    it {should have_many(:snacks).through(:machine_snacks)}
+  end
+
+  describe "instance methods" do
+    it "average snack price for machine" do
+      sam = Owner.create(name: "Sam's Snacks")
+      basement = sam.machines.create(location: "Turing Basement")
+      snack_1 = basement.snacks.create(name: "Cheetos", price: 1.50)
+      snack_2 = basement.snacks.create(name: "Chocolate", price: 2.50)
+
+      expect(basement.average_snack_price).to eq(2.00)
+    end
+
+    it "total snack count for machine" do
+      sam = Owner.create(name: "Sam's Snacks")
+      basement = sam.machines.create(location: "Turing Basement")
+      snack_1 = basement.snacks.create(name: "Cheetos", price: 1.50)
+      snack_2 = basement.snacks.create(name: "Chocolate", price: 2.50)
+
+      expect(basement.total_snack_count).to eq(2)
+    end
   end
 end
